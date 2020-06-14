5a7f9e3e1ecc1f169c3d3fc350d0d6ea679eb31b
Zahra Deen <zahradeenie@gmail.com>
2020-06-14 18:09:37 +0200

    scaffold vue frontend

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <p>hello</p>
+  <div class="container">
+    <div class="g-signin2"></div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "app",
-  data() {
-    return {};
-  },
-  methods: {}
+  name: "App"
 };
 </script>
 
@@ -17,5 +15,13 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+
+.container {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
