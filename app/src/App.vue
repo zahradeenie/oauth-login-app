b810767f9266cde34527dc784b6e2622e3def92e
Zahra Deen <zahradeenie@gmail.com>
2020-06-19 09:56:23 +0200

    fix: remove google oauth

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,48 +1,14 @@
 <template>
-  <div class="container">
-    <div class="g-signin2" data-onsuccess="onSignIn"></div>
-  </div>
+  <div class="container"></div>
 </template>
 
 <script>
 export default {
   name: "App",
   data() {
-    return {
-      clientId:
-        "880555996596-sa59cbnaepk7qknt8cddg8uf52u5is5v.apps.googleusercontent.com",
-      redirectUri: "http://localhost:8080"
-    };
+    return {};
   },
-  methods: {
-    oauthSignIn() {
-      const oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
-
-      const form = document.createElement("form");
-      form.setAttribute("method", "GET");
-      form.setAttribute("action", oauth2Endpoint);
-
-      const params = {
-        client_id:
-          "880555996596-sa59cbnaepk7qknt8cddg8uf52u5is5v.apps.googleusercontent.com",
-        redirect_uri: "http://localhost:8080",
-        response_type: "token",
-        scope: "https://www.googleapis.com/auth/drive.metadata.readonly",
-        include_granted_scopes: "true",
-        state: "pass-through value"
-      };
-
-      for (let param in params) {
-        const input = document.createElement("input");
-        input.setAttribute("type", "hidden");
-        input.setAttribute("name", param);
-        input.setAttribute("value", params[param]);
-        form.appendChild(input);
-      }
-      document.body.appendChild(form);
-      form.submit();
-    }
-  }
+  methods: {}
 };
 </script>
 
