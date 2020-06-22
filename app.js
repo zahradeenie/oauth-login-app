638f6eb31262c2a999372e65da7cb69f25395980
Zahra Deen <zahradeenie@gmail.com>
2020-06-22 18:31:03 +0200

    feat: get github user

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-const Koa = require('koa');
-const koaRouter = require('koa-router');
-const logger = require('koa-logger');
+const express = require('express');
+const bodyParser = require("body-parser");
+const route = require('./middleware/index');
+require('dotenv').config();
 
-const app = new Koa();
+const app = express();
 const port = 5000;
 
-// logs events to terminal
-app.use(logger());
+const clientId = process.env.GITHUB_CLIENT_ID;
+const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+let token = null;
 
-// error handling
-app.use(async (ctx, next) => {
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.get('/', (req, res) => {
+  res.status(200).send({ msg: "hello" });
+});
+
+app.get('/login/github', (req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=user`)
+});
+
+app.get('/oauth-callback', async (req, res) => {
+  const code = req.query.code;
+  const accessToken = await route.getAccessToken({ code, clientId, clientSecret });
+
+  if (!accessToken) {
+    res.status(401).send({ msg: 'Unauthorized' });
+  };
+
+  token = accessToken
+
   try {
-   await next(); 
-  } catch (err) {
-    ctx.status = err.status || 500;
-    ctx.body = err.message;
-    ctx.app.emit('error', err, ctx);
+    const { data: user } = await route.fetchGithubUser(accessToken);
+    res.status(200).send({ ok: user });
+  } catch (error) {
+    res.status(500).send({ msg: error.message });
+    console.log(error);
   }
 });
 
-// new instance of router and routes
-const router = new koaRouter();
-
-router.get('/', (ctx, next) => {
-  console.log(ctx);
-  ctx.body = ctx
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-const server = app.listen(port);
-
+app.listen(port);
 console.log(`Listening on port: ${port}`);
-
-module.exports = server;
-
